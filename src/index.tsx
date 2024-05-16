d5ec8f13411e593a92eea8decdab3c1132b7174a
ke-kimura <kimura7298@yahoo.co.jp>
2024-05-16 21:07:51 +0900

    first

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const View = () => {
+  return (
+    <html>
+      <body>
+        <h1>Hello Hono!</h1>
+      </body>
+    </html>
+  )
+}
+
+app.get('/page', (c) => {
+  return c.html(<View />)
+})
+export default app
