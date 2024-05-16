98ced3a272b0ccdf86e7e8d40b2946eef0b0932f
ke-kimura <kimura7298@yahoo.co.jp>
2024-05-16 21:10:13 +0900

    basic auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
+
 
 const app = new Hono()
 
-const View = () => {
-  return (
-    <html>
-      <body>
-        <h1>Hello Hono!</h1>
-      </body>
-    </html>
-  )
-}
+app.use(
+  '/admin/*',
+  basicAuth({
+    username: 'admin',
+    password: 'secret',
+  })
+)
 
-app.get('/page', (c) => {
-  return c.html(<View />)
+app.get('/admin', (c) => {
+  return c.text('Your are authorized!')
 })
+
 export default app
